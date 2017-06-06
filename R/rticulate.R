b135f7e691d84956c2e9d9216996a258828a3e96
Stefano Coretta <stefanoc.social@me.com>
2017-06-06 16:29:38 +0200

    add package import

--- R/rticulate.R
+++ R/rticulate.R
@@ -9,6 +9,7 @@
 #' @param column.names A character vector with the names of the columns.
 #' @param fan.lines A number vector with the number of fan lines (the default is 42).
 #' @param na.rm A boolean string (the default is `FALSE`).
+#' @importFrom magrittr "%>%"
 read_aaa <- function(file, column.names, fan.lines = 42, na.rm = FALSE) {
     columns <- c(
         column.names,
@@ -18,16 +19,16 @@ read_aaa <- function(file, column.names, fan.lines = 42, na.rm = FALSE) {
         )
     )
 
-    data <- read_tsv(
+    data <- readr::read_tsv(
         file,
         col_names = columns,
         na = "*",
         trim_ws = TRUE
     ) %>%
-        mutate_at(vars(matches("^[XY]_")),funs(as.numeric)) %>%
-        gather(spline, coordinate, matches("[XY]_")) %>%
-        separate(spline, c("axis", "fan"), convert = TRUE) %>%
-        spread(axis, coordinate)
+        dplyr::mutate_at(dplyr::vars(dplyr::matches("^[XY]_")), dplyr::funs(as.numeric)) %>%
+        tidyr::gather(spline, coordinate, dplyr::matches("[XY]_")) %>%
+        tidyr::separate(spline, c("axis", "fan"), convert = TRUE) %>%
+        tidyr::spread(axis, coordinate)
 
     if (na.rm == TRUE) {
         data <- na.omit(data)
@@ -42,7 +43,7 @@ read_aaa <- function(file, column.names, fan.lines = 42, na.rm = FALSE) {
 #'
 #' @param data A data frame.
 plot_splines <- function(data) {
-    ggplot(data, aes(X, Y)) +
+    ggplot2::ggplot(data, aes(X, Y)) +
         geom_smooth(method = "loess", se = FALSE) +
         coord_fixed(ratio = 1) +
         labs(x = "antero-posterior",
