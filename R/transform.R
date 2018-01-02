deb796679030ae608b07b16ab539b4f72a9102e1
Stefano Coretta <stefanoc.social@me.com>
2018-01-02 21:36:48 +0100

    add get_origin
    Need to make it more robust: it throws NAs if there are NAs

--- R/transform.R
+++ R/transform.R
@@ -13,6 +13,30 @@ transform_coordinates <- function(data, to = "polar", origin = NULL) {
     }
 }
 
-get_origin <- function(data) {
+get_origin <- function(data, fan_line_col = "fan_line", fan_lines = c(10, 25)) {
+    x_1 <- c(
+        data$X[data[[fan_line_col]] == fan_lines[1]][1],
+        data$X[data[[fan_line_col]] == fan_lines[1]][2]
+    )
+    y_1 <- c(
+        data$Y[data[[fan_line_col]] == fan_lines[1]][1],
+        data$Y[data[[fan_line_col]] == fan_lines[1]][2]
+    )
 
+    x_2 <- c(
+        data$X[data[[fan_line_col]] == fan_lines[2]][1],
+        data$X[data[[fan_line_col]] == fan_lines[2]][2]
+    )
+    y_2 <- c(
+        data$Y[data[[fan_line_col]] == fan_lines[2]][1],
+        data$Y[data[[fan_line_col]] == fan_lines[2]][2]
+    )
+
+    denominator <- ((x_1[1] - x_1[2]) * (y_2[1] - y_2[2]) - (y_1[1] - y_1[2]) * (x_2[1] - x_2[2]))
+
+    x <- ((x_1[1]*y_1[2] - y_1[1]*x_1[2]) * (x_2[1] - x_2[2]) - (x_1[1] - x_1[2]) * (x_2[1] * y_2[2] - y_2[1] * x_2[2])) / denominator
+
+    y <- ((x_1[1]*y_1[2] - y_1[1]*x_1[2]) * (y_2[1] - y_2[2]) - (y_1[1] - y_1[2]) * (x_2[1] * y_2[2] - y_2[1] * x_2[2])) / denominator
+
+    return(c(x, y))
 }
