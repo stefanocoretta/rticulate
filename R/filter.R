87832776673d226dd7613e68a4cf6743be6e3f9c
stefanocoretta <stefano.coretta@gmail.com>
2024-04-23 18:08:52 +0100

    add apply argument to filter signal

--- R/filter.R
+++ R/filter.R
@@ -21,16 +21,23 @@ filter_signal <- function(
   window_length = NULL,
   cutoff_freq = NULL,
   sampling_freq = NULL,
-  type = "low"
+  type = "low",
+  apply = 1
 ) {
-  if (filter == "sgolay") {
-    signal_sm <- gsignal::sgolayfilt(signal, p = order, n = window_length)
-  } else if (filter == "butter") {
-    nyquist_freq <- sampling_freq / 2
-    normalized_cutoff <- cutoff_freq / nyquist_freq
-    butter_filt <- gsignal::butter(order, normalized_cutoff, type = type)
 
-    signal_sm <- gsignal::filtfilt(butter_filt, signal)
+  signal_sm <- signal
+
+  for (iter in 1:apply) {
+    if (filter == "sgolay") {
+      signal_sm <- gsignal::sgolayfilt(signal_sm, p = order, n = window_length)
+    } else if (filter == "butter") {
+      nyquist_freq <- sampling_freq / 2
+      normalized_cutoff <- cutoff_freq / nyquist_freq
+      butter_filt <- gsignal::butter(order, normalized_cutoff, type = type)
+
+      signal_sm <- gsignal::filtfilt(butter_filt, signal_sm)
+    }
+
   }
 
   return(signal_sm)
